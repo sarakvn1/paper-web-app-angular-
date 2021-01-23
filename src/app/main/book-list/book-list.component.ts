faeac8f8f1d4fa5a6356caf503727c6d0b5b85ac
sarakvn1 <sarakvn@gmail.com>
2021-01-23 13:06:52 -0800

    cookies

--- src/app/main/book-list/book-list.component.ts
+++ src/app/main/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Input,EventEmitter,Output } from '@angular/core';
 import { ApiService } from 'app/shared/services/api.service';
-
+import { MessageService } from 'app/shared/services/message.service';
+import * as moment from 'moment';
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
@@ -14,19 +15,49 @@ export class BookListComponent implements OnInit {
   books:any
   selectedBook:null
   constructor(
-    private apiService:ApiService
+    private apiService:ApiService,
+    private messageService:MessageService
     ) { }
+    sendMessage(message): void {
+      // send message to subscribers via observable subject
+      this.messageService.sendMessage(message);
+  }
+
+  clearMessages(): void {
+      // clear messages
+      this.messageService.clearMessages();
+  }
   Genres=[]
   
   ngOnInit(){
     
     
-    this.Genres=this.apiService.getGenres()
+    // this.Genres=this.apiService.getGenres()
     this.apiService.getBooks().subscribe(
       data=>{
         this.books=data
       },
       error=>console.log(error))
+  }
+
+  addToCard(book){
+
+    var today=moment().format();  
+     const order={
+      book_id:book.id,
+      quantity:1,
+      date:today
+    }
+    this.sendMessage("one item added")
+    console.log("this is book id",book.id)
+    this.apiService.getOrders(order)
+    this.apiService.senOrderItems().subscribe(
+      data=>{
+        console.log(data)
+        
+      },
+      error=>console.log(error))
+    
   }
   // bookClicked(book){
   //   this.selectBook.emit(book)
